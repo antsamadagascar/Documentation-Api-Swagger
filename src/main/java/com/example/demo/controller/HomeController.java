74043101485a418be4271a285af9e585e94995d2
Aina Ny Antsa Ratovonandrasana <nyantsa.ratovo@gmail.com>
2025-05-30 20:01:33 +0300

    feat:default redirection in the login page

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,15 +1,15 @@
 package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/")
 public class HomeController {
 
     @GetMapping
     public String home() {
-        return "Bienvenue sur mon API personnaliser en Spring Boot!";
+        return "redirect:/login";
     }
 }
