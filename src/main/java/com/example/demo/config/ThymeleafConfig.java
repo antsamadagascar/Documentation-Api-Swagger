1fc97042b5a2ca4cc40429e7a163e286b153d8b8
Aina Ny Antsa Ratovonandrasana <nyantsa.ratovo@gmail.com>
2025-05-30 14:15:40 +0300

    Add spring boot template to the base of the project.

---
+++ src/main/java/com/example/demo/config/ThymeleafConfig.java
@@ -0,0 +1,19 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
+@Configuration
+public class ThymeleafConfig {
+
+    @Bean
+    public SpringTemplateEngine templateEngine(ITemplateResolver templateResolver) {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver);
+        templateEngine.setEnableSpringELCompiler(true);
+        templateEngine.addTemplateResolver(templateResolver);
+        return templateEngine;
+    }
+}
